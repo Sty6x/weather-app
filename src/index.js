3b68c9c7710e76fe8ba0b65528145e5c813a5762
zaid <jerylfranz460@gmail.com>
2022-10-07 15:51:11 +0400

    test-dev

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+console.log('present')
+console.log('hey')
+const body = document.querySelector('body')
+const header = document.createElement('h1')
+header.textContent = ' HEY HOY'
+body.appendChild(header)
