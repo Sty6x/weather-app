fc543aa0535d4fea19c175721a8a049e90051607
zaid <jerylfranz460@gmail.com>
2022-10-14 01:01:32 +0400

    made functions to change classes

--- src/dropDown.js
+++ src/dropDown.js
@@ -6,8 +6,36 @@ console.log('drop down initialize');
 // third argument is the trigger, it needs to be specific,
 // this will need a query selector
 
+const activeMenu = {
+  position: 'relative',
+  transform: 'translate(1, 15 %)',
+  transition: 'transform 200ms ease',
+  animation: '300ms menu-fade-in ease'
+}
+
+const inactiveMenu = {
+  transform: 'translate(0, -30%)',
+  transition: 'transform 200ms ease',
+  animation: '101ms menu-fade-out ease',
+  opacity: '0'
+}
+
+export function changeInactive(list) {
+  if (list.classlist.contains('active-menu')) {
+    list.classList.remove('inactive-menu')
+    list.setattribute('class', 'inactive-menu')
+  }
+}
+export function changeActive(list) {
+  if (list.classlist.contains('active-menu')) {
+    list.classList.remove('active-menu')
+    list.setattribute('class', 'inactive-menu')
+  }
+}
+
 export function dropDown(target, list, trigger) {
   const menuList = document.getElementById(list)
+  menuList.setAttribute('class', 'inactive-menu')
   if (target.matches('li')) {
     target.setAttribute('style', 'background-color:blue;')
   }
