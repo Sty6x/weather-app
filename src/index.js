da2069b95749e98afa5be3e05f17d0bb76be50b7
zaid <jerylfranz460@gmail.com>
2022-10-18 01:25:56 +0400

    yes

--- src/index.js
+++ src/index.js
@@ -3,25 +3,27 @@ import './style.css'
 
 const mainItem = document.getElementById('main-item');
 const rudder = document.getElementById('rudder-item-list')
+const leftRud = document.getElementById('left-rud-item');
+const rightRud = document.getElementById('right-rud-item');
 
-mainItem.addEventListener('click', (e) => {
-  if (!rudder.classList.contains('rudder-inactive')) {
-    rudder.removeAttribute('class', 'rudder-active')
-    rudder.classList.add('rudder-inactive')
-    for (let i = 0; i < rudder.children.length; i++) {
-      console.log('ha')
-      if (rudder.children[i].id == 'main-item') {
-        console.log(rudder.children[i])
-        rudder.children[i].setAttribute('style', 'display:flex;')
-      } else {
-        rudder.children[i].setAttribute('style', 'display:none;')
-      }
-    }
-  } else if (rudder.classList.contains('rudder-inactive')) {
-    rudder.removeAttribute('class', 'rudder-inactive')
-    rudder.classList.add('rudder-active')
-    for (let i = 0; i < rudder.children.length; i++) {
-      rudder.children[i].setAttribute('style', 'display:flex;')
+
+rudder.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.matches('li')) {
+    if (!rudder.classList.contains('rudder-inactive')) {
+      rudder.removeAttribute('class', 'rudder-active')
+      rightRud.classList.remove('right-rud-active')
+      leftRud.classList.remove('left-rud-active')
+      rudder.classList.add('rudder-inactive')
+      rightRud.classList.add('right-rud-inactive')
+      leftRud.classList.add('left-rud-inactive')
+    } else if (rudder.classList.contains('rudder-inactive')) {
+      rudder.removeAttribute('class', 'rudder-inactive')
+      rightRud.classList.remove('right-rud-inactive')
+      leftRud.classList.remove('left-rud-inactive')
+      rudder.classList.add('rudder-active')
+      rightRud.classList.add('right-rud-active')
+      leftRud.classList.add('left-rud-active')
     }
   }
 })
