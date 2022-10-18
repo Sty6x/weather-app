2b0447a684c89c6604e0966dc50cea08359e46d0
zaid <jerylfranz460@gmail.com>
2022-10-18 05:57:47 +0400

    fully functioning rudder navigation

--- src/index.js
+++ src/index.js
@@ -9,21 +9,31 @@ const rightRud = document.getElementById('right-rud-item');
 
 rudder.addEventListener('click', (e) => {
   const target = e.target;
+  rudderAnimation(target)
+})
+
+function rudderAnimation(target) {
   if (target.matches('li')) {
     if (!rudder.classList.contains('rudder-inactive')) {
+
       rudder.removeAttribute('class', 'rudder-active')
+      rudder.classList.add('rudder-inactive')
+
       rightRud.classList.remove('right-rud-active')
       leftRud.classList.remove('left-rud-active')
-      rudder.classList.add('rudder-inactive')
       rightRud.classList.add('right-rud-inactive')
       leftRud.classList.add('left-rud-inactive')
+
     } else if (rudder.classList.contains('rudder-inactive')) {
+
       rudder.removeAttribute('class', 'rudder-inactive')
+      rudder.classList.add('rudder-active')
+
       rightRud.classList.remove('right-rud-inactive')
       leftRud.classList.remove('left-rud-inactive')
-      rudder.classList.add('rudder-active')
       rightRud.classList.add('right-rud-active')
       leftRud.classList.add('left-rud-active')
+
     }
   }
-})
+}
