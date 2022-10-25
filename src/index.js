9e7fce572c3e2bcdc7d4cbc124db687073de36e0
zaid <jerylfranz460@gmail.com>
2022-10-25 15:25:51 +0400

    testing Response Object methods and extracting specific data from the body

--- src/index.js
+++ src/index.js
@@ -12,25 +12,29 @@ main.append(btn, search)
 function getImg() {
   return fetch(`https://api.giphy.com/v1/gifs/translate?api_key=6C6Bv7RmIvF06uPUz6RVMaQgiWxSQiKd&s=${search.value}`, { mode: 'cors' })
     .then((response) => {
+      console.log(response.blob())
       return response.json();
     }).then((response) => {
-      console.log(response)
-      console.log(response.data.images.original.url)
       image.src = response.data.images.original.url
-    }).catch(e => {
+    }).catch(() => {
       console.log("images doesn't exist")
       return fetch(`https://api.giphy.com/v1/gifs/translate?api_key=6C6Bv7RmIvF06uPUz6RVMaQgiWxSQiKd&s=nothing`, { mode: 'cors' })
         .then(errorImg => {
           return errorImg.json();
         }).then(errorImg => {
-          console.log('ee')
           image.src = errorImg.data.images.original.url
         }).catch(e => {
+          console.log(e)
           console.log('default image doesnt exist')
         })
     })
 }
-search.addEventListener('input', () => {
-  console.log(search.value)
-})
+function test() {
+  return fetch(`https://api.giphy.com/v1/gifs/translate?api_key=6C6Bv7RmIvF06uPUz6RVMaQgiWxSQiKd&s=${search.value}`, { mode: 'cors' })
+    .then((response) => {
+      console.log(response.text())
+      // return response.blob()
+    })
+}
+test()
 btn.addEventListener('click', getImg)
