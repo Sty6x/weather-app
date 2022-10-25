92034eb0096063aeef329301f0d3d91398f3fedc
zaid <jerylfranz460@gmail.com>
2022-10-25 13:44:28 +0400

    fetching data using giphy's api (its fun)

--- src/index.js
+++ src/index.js
@@ -1,94 +1,36 @@
 import './style.css'
 
-// const main = document.querySelector('nav')
-// const rudderContainer = document.createElement('div');
-// const rudderItemList = document.createElement('ul')
-// const leftRudItem = document.createElement('div')
-// const rightRudItem = document.createElement('div')
-// const locLi = document.createElement('li')
-// const msgLi = document.createElement('li')
-// const mainItmLi = document.createElement('li')
-// const userLi = document.createElement('li')
-// const settingsLi = document.createElement('li')
 
+const image = document.querySelector('img')
+const main = document.querySelector('main')
+const btn = document.createElement('button')
+const search = document.createElement('input')
+search.setAttribute('type', 'text')
+btn.textContent = 'Get Image'
+main.append(btn, search)
 
-
-// export function createNav(container) {
-//   leftRudItem.append(locLi, msgLi)
-//   rightRudItem.append(userLi, settingsLi)
-//   rudderItemList.append(leftRudItem, mainItmLi, rightRudItem)
-//   rudderContainer.appendChild(rudderItemList)
-//   container.appendChild(rudderContainer)
-//   applyIDClassAttr()
-// }
-
-
-// function setIDClass(element, id, cls) {
-//   const attributes = {
-//     id,
-//     class: cls
-//   }
-//   for (let key in attributes) {
-//     element.setAttribute(key, attributes[key])
-//   }
-// }
-
-// function applyIDClassAttr() {
-//   setIDClass(rudderContainer, 'rudder-container', '')
-//   setIDClass(rudderItemList, 'rudder-item-list', 'rudder-active')
-//   setIDClass(leftRudItem, 'left-rud-item', 'left-rud-active')
-//   setIDClass(rightRudItem, 'right-rud-item', 'right-rud-active')
-//   setIDClass(locLi, 'loc', '')
-//   setIDClass(msgLi, 'msg', '')
-//   setIDClass(mainItmLi, 'main-item', '')
-//   setIDClass(userLi, 'user', '')
-//   setIDClass(settingsLi, 'settings', '')
-// }
-
-// // fix this so that we can export this function
-// // module will provide the properties needed since
-// // the this module will create a rudder navigation once the
-// // create nav is invoked
-
-// export function rudderAnimation(target) {
-//   if (target.matches('li')) {
-//     if (!rudderItemList.classList.contains('rudder-inactive')) {
-
-//       rudderItemList.removeAttribute('class', 'rudder-active')
-//       rudderItemList.classList.add('rudder-inactive')
-
-//       rightRudItem.classList.remove('right-rud-active')
-//       leftRudItem.classList.remove('left-rud-active')
-//       rightRudItem.classList.add('right-rud-inactive')
-//       leftRudItem.classList.add('left-rud-inactive')
-
-//     } else if (rudderItemList.classList.contains('rudder-inactive')) {
-
-//       rudderItemList.removeAttribute('class', 'rudder-inactive')
-//       rudderItemList.classList.add('rudder-active')
-
-//       rightRudItem.classList.remove('right-rud-inactive')
-//       leftRudItem.classList.remove('left-rud-inactive')
-//       rightRudItem.classList.add('right-rud-active')
-//       leftRudItem.classList.add('left-rud-active')
-//     }
-//   }
-// }
-
-// rudderContainer.addEventListener('click', (e) => {
-//   const target = e.target;
-//   rudderAnimation(target)
-// })
-
-// createNav(main)
-
-
-// import * as RudNav from '@sty6x/rudder-nav'
-// const main = document.querySelector('main')
-// const nav = document.querySelector('nav')
-
-// RudNav.createNav(nav)
-// main.addEventListener('click', e => {
-//   const target = e.target;
-//   RudNav.rudderAnimation(target)
-// })
+function getImg() {
+  return fetch(`https://api.giphy.com/v1/gifs/translate?api_key=6C6Bv7RmIvF06uPUz6RVMaQgiWxSQiKd&s=${search.value}`, { mode: 'cors' })
+    .then((response) => {
+      return response.json();
+    }).then((response) => {
+      console.log(response)
+      console.log(response.data.images.original.url)
+      image.src = response.data.images.original.url
+    }).catch(e => {
+      console.log("images doesn't exist")
+      return fetch(`https://api.giphy.com/v1/gifs/translate?api_key=6C6Bv7RmIvF06uPUz6RVMaQgiWxSQiKd&s=nothing`, { mode: 'cors' })
+        .then(errorImg => {
+          return errorImg.json();
+        }).then(errorImg => {
+          console.log('ee')
+          image.src = errorImg.data.images.original.url
+        }).catch(e => {
+          console.log('default image doesnt exist')
+        })
+    })
+}
+search.addEventListener('input', () => {
+  console.log(search.value)
+})
+btn.addEventListener('click', getImg)
