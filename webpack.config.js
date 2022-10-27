f724216307c51465922f274520bd781971c9cc6c
zaid <jerylfranz460@gmail.com>
2022-10-27 08:59:39 +0400

    added place holders

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,14 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      }, {
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
     ],
   },
