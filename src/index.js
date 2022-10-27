4c9a4983856b25d077783b73a2fae883de91c38e
zaid <jerylfranz460@gmail.com>
2022-10-27 10:30:39 +0400

    attempting to use geolocation

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import * as Weather from './weather-data.js'
 import * as Display from './display-weather.js'
 
 const userInput = document.querySelector('input')
-const btn = document.getElementById('search')
-const triggers = ['click', 'keypress']
-
 
 userInput.addEventListener('keypress', e => {
   if (e.key == 'Enter') {
     Display.displayWeather(Weather.getWeather, userInput)
   }
 })
+
+
+
+console.log(Geolocation.getCurrentPosition())
+window.onload(Display.displayWeather(Weather.getWeather, ''))
